58c90d4dc943147646f39b38f0baeec28d1896c6
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2025-02-08 19:40:45 +0530

    trying size

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className='w-[60%] w-90% sm:p-4 px-4 py-4 text-[1rem] text-gray-800  border-t border-gray-400 mx-auto mt-2'>
+    <section className='sm:w-[60%] w-[90%] sm:p-4 px-4 py-4 text-[1rem] text-gray-800  border-t border-gray-400 mx-auto mt-2'>
       <p 
       className='text-center font-semibold'
       >Copyright 2025 | Ujjwal Kumar</p>
