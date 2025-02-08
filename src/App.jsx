1ff345f68992a42f3abaddef3a6a7b2d3b5a3fcd
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2025-02-08 16:36:45 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/header/Header"
+import HeroSection from "./components/hero-section/HeroSection"
+
+
+
+function App() {
+  
+
+  return (
+    <div className="bg-[#f8f9fa]">
+    <Header />
+    <HeroSection />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='h-3'>
+      
+    </div>
+  )
+}
+
+export default Header
