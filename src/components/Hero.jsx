95263b2dcad5908d4fe7249a2a2da70d2cb6c80e
UjjwalKumar02 <kumarujjwaldos@gmail.com>
2025-11-08 18:18:49 +0530

    Changed styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,36 +6,38 @@ const Hero = () => {
   return (
     <div
       id="about"
-      className="lg:w-[60%] w-[92%] bg-white mx-auto flex justify-between items-center lg:p-10 p-8 rounded-xl shadow-sm border-[1.5px] border-gray-300 mt-10"
+      className="lg:max-w-4xl max-w-[91%] mx-auto bg-white lg:p-10 p-8 flex flex-col-reverse gap-10 sm:flex-row justify-between items-center rounded-2xl shadow-sm border-[1.5px] border-gray-300 mt-10"
     >
-      <div className="md:max-w-[66%] space-y-5">
-        <p className="lg:text-2xl text-xl font-medium md:text-left text-center">
+      <div className="md:max-w-[60%] md:space-y-4 space-y-2">
+
+        <h2 className="lg:text-2xl text-xl font-medium md:text-left text-center">
           Ujjwal kumar
-        </p>
-        <p className="text-gray-700 lg:text-left text-center lg:text-[0.91rem] text-sm">
-          Final year B.Tech student at MAIT, Delhi, and Aspiring Software
+        </h2>
+
+        <p className="md:text-left text-center lg:text-base text-sm">
+          B.Tech student at MAIT, and Web
           developer.
         </p>
 
-        <div className="flex gap-2 md:justify-start justify-center items-center lg:mt-6 mt-8 text-sm">
-          <a
-            href="mailto:ujjwal.kumar.id@gmail.com"
-            className="flex gap-1.5 items-center bg-black text-white px-5 py-[5.1px] rounded-xl "
-          >
-            <IoMdMail size={16}/> Mail
-          </a>
+        <div className="md:max-w-[40%] flex gap-2 md:justify-start justify-center items-center mt-8 text-sm">
           <a
             href="https://github.com/ujjwalkumar02"
-            className="flex gap-1.5 items-center bg-black text-white px-5 py-[5px] rounded-xl"
+            className="w-full px-5 py-2 flex gap-1.5 justify-center items-center bg-black text-white rounded-lg"
             target="_blank"
           >
             <FaGithub size={16}/> Github
           </a>
+          <a
+            href="mailto:ujjwal.kumar.id@gmail.com"
+            className="px-5 py-2 flex gap-1.5 justify-center items-center bg-white text-black rounded-lg border-[1.5px] border-gray-400"
+          >
+            <IoMdMail size={16}/> Mail
+          </a>
         </div>
       </div>
 
-      <div className="md:block hidden">
-        <img src={img} alt="" className="w-49 rounded-xl " />
+      <div className="">
+        <img src={img} alt="profile-image" className="md:w-49 w-34 rounded-2xl object-contain" />
       </div>
     </div>
   );
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 
+
 const Nav = () => {
   const [isSticky, setIsSticky] = useState(false);
 
@@ -7,34 +8,35 @@ const Nav = () => {
     const handleScroll = () => {
       setIsSticky(window.scrollY > 0);
     };
-
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
-  return (
-    <div
-      className={`sticky lg:top-1 top-0 lg:w-[57%] flex justify-between items-center mx-auto lg:py-3.5 lg:rounded-xl lg:px-10 px-5.5 py-5.5 ${
-        isSticky
-          ? "bg-white shadow-sm lg:border-[1.5px] border-b-[1.5px] lg:border-gray-400 border-gray-300"
-          : "bg-transparent"
-      }`}
-    >
-      <a href="#about" className="text-xl font-semibold">Ujjwal</a>
 
-      <div className="gap-10 text-sm font-medium text-gray-600 md:flex hidden items-center">
-        <a href="#about">About</a>
+  return (
+    <nav
+      className={`sticky top-0 max-w-6xl mx-auto px-5 py-4 flex justify-between items-center lg:rounded-lg 
+        ${isSticky
+          ? "bg-white shadow-sm border-b-[1.5px] border-gray-300"
+          : "bg-transparent"
+        }`}
+    >
+      <a href="/">
+        <h1 className="text-xl font-semibold tracking-tighter">Ujjwal</h1>
+      </a>
+
+      <div className="flex items-center gap-6 text-sm">
         <a href="#projects">Projects</a>
-        <a href="#skills">Skills</a>
+        <a
+          href="#contact"
+          className="bg-black text-white px-4 py-2 rounded-lg"
+        >
+          Contact
+        </a>
       </div>
 
-      <a
-        href="#contact"
-        className="bg-black text-white px-4 py-[5px] rounded-xl text-sm"
-      >
-        Contact
-      </a>
-    </div>
+
+    </nav>
   );
 };
 
